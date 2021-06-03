a7f7f5a9fce4fd311ca8a4547040decb02228486
AniruddhNarayanan <72774377+AniruddhNarayanan@users.noreply.github.com>
2021-06-03 16:17:26 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is where I declared the variables
+description= "My Favorite Band"
+band = "The Eagles"
+date_formed = 1971
+favourite_song = "Hotel California"  
+
+"""
+  this is where I put the print function so you can see the output
+    """
+print(description)
+print(band)
+print(date_formed)
+print(favourite_song)
